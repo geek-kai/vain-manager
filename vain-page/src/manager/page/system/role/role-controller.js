02f8b12fd4d52a6b292a77c758224234ad73d213
vainhope <845869847@qq.com>
2017-10-10 14:56:22 +0800

    角色

--- vain-page/src/manager/page/system/role/role-controller.js
+++ vain-page/src/manager/page/system/role/role-controller.js
@@ -2,11 +2,17 @@ angular.module("role.controllers", []).controller(
     "RoleListCtrl", ["appConstant", "$scope", "roleHttpServices", "commonUtils", "msgModal",
         function (appConstant, $scope, roleHttpServices, commonUtils, msgModal) {
             $scope.init = function () {
-
                 roleHttpServices.getList({id: 1}, function (data) {
-                   if(data.code==200){
-                      $scope.roles =  data.dataList;
-                   }
+                    if (data.code == 200) {
+                        $scope.roles = data.dataList;
+                    }
                 });
+            };
+
+            $scope.addRole = function () {
+                commonUtils.showCover();
+                $scope.coverShow = 1;
+                $scope.addWindow = 1;
+                commonUtils.centerElem($("#modifyDiv"));
             };
         }]);
