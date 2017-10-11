63142705c53aa6a2685bab5588a2b2232506d570
vain <845869847@qq.com>
2017-10-11 20:40:13 +0800

    菜单列表展示

--- vain-page/src/manager/page/system/menu/menu-controller.js
+++ vain-page/src/manager/page/system/menu/menu-controller.js
@@ -5,9 +5,28 @@ angular.module("menu.controllers", [])
             $scope.init = function () {
                 menuHttpServices.getMenuList({}, function (data) {
                     if (data.code == 200) {
-                        $scope.menus = data.dataList;
+                        if (commonUtils.isArray(data.dataList)) {
+                            $scope.menus = new Array();
+                            $scope.converterMenus(data.dataList);
+                        }
                     }
                 });
+            };
+
+            /*将菜单数据转换*/
+            $scope.converterMenus = function (dataList) {
+                for (var i = 0; i < dataList.length; i++) {
+                    dataList[i].isShow = true;
+                    $scope.menus.push(dataList[i]);
+                    if (commonUtils.isArray(dataList[i].children)) {
+                        dataList[i].isShowChildren = false;//默认不展开子类
+                        dataList[i].hasChildren = true;
+                        $scope.converterMenus(dataList[i].children);
+                        /*for (var j = 0; j < dataList[i].children.length; j++) {
+                         }*/
+                    }
+                }
+
             };
 
         }]);
